bb0243b0e215509a303818ce38609923b74522a8
Hamza Çeçen <93983281+HamzaCecen@users.noreply.github.com>
2023-07-25 11:04:48 +0300

    Add files via upload

---
+++ Models/forStopWatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DispactherTimePlan.Models
+{
+    class forStopWatch
+    {
+        
+        private string foo1;
+
+        public string foo
+        {
+            get { return foo1;  }
+            set { foo1 = value;  }
+        }
+        
+
+        //public string foo { get; set; }
+    }
+
+    
+
+}
